e0897ffe5323d8d707e22da477febaa3435acbdd
kashalol <kashagol@gmail.com>
2017-12-24 23:15:49 +0200

    For UITSchool

--- com/company/lesson5/homeWork/Exercise16TwoDimensional5x8.java
+++ com/company/lesson5/homeWork/Exercise16TwoDimensional5x8.java
@@ -11,7 +11,7 @@ public class Exercise16TwoDimensional5x8 {
 
     public static int[][] createBasicArray() {
         int[][] arr = new int[5][8];
-        int i, j= 0;
+        int i, j;
         for (i = 0; i < 5; i++) {
             for (j = 0; j < 8; j++) {
                 int toArray = (int) (Math.random() * 10);
