b77a63b668bf2ac534223560d212adf1dbc342ff
kashalol <kashagol@gmail.com>
2018-01-28 09:05:03 +0200

    For UITSchool

--- com/company/lesson7/homeWork/Library.java
+++ com/company/lesson7/homeWork/Library.java
@@ -42,9 +42,19 @@ class ReaderTicket {
 
 class Book {
     String bookName;
+    private static int amount = 0;
 
     public Book(String name) {
         this.bookName = name;
+        amount++;
+    }
+
+    static int getAmount() {
+        return amount;
+    }
+
+    public String getBookName() {
+        return bookName;
     }
 }
 
@@ -52,8 +62,18 @@ public class Library {
     public static void main(String[] args) {
         ReaderTicket readerTicket1 = new ReaderTicket("John Smithson", "01", "Java", "01.01.1999", "3720001");
         ReaderTicket readerTicket2 = new ReaderTicket("Smith Johnson", "02", "C++", "02.02.1999", "3720002");
+        Book book1 = new Book("Азбука");
+        Book book2 = new Book("Пушкин А.С");
+        Book book3 = new Book("Head First Java");
         ReaderTicket readerTicket3 = fillingReaderTicket03();
         ReaderTicket.arrayOfTickets(readerTicket1.readerTicketOut(), readerTicket2.readerTicketOut(), readerTicket3.readerTicketOut());
+        takeBook(Book.getAmount());
+        takeBook(book1, book2, book3);
+        takeBook(book1.getBookName(),book2.getBookName(),book3.getBookName());
+        returnBook(Book.getAmount());
+        returnBook(book1, book2, book3);
+        returnBook(book1.getBookName(),book2.getBookName(),book3.getBookName());
+
     }
 
     public static ReaderTicket fillingReaderTicket03() {
@@ -71,21 +91,27 @@ public class Library {
         return readerTicket3;
     }
 
-    public void takeBook(int amount) {
+    public static void takeBook(int amount) {
+        System.out.println("Было взято " + amount + " книг");
     }
 
-    public void takeBook(String... names) {
+    public static void takeBook(String... names) {
+        System.out.println("Было взято " + names.length + " книг");
     }
 
-    public void takeBook(Book... books) {
+    public static void takeBook(Book... books) {
+        System.out.println("Было взято " + books.length + " книг");
     }
 
-    public void returnBook(int amount) {
+    public static void returnBook(int amount) {
+        System.out.println("Было возвращено " + amount + " книг");
     }
 
-    public void returnBook(String... names) {
+    public static void returnBook(String... names) {
+        System.out.println("Было возвращено " + names.length + " книг");
     }
 
-    public void returnBook(Book... books) {
+    public static void returnBook(Book... books) {
+        System.out.println("Было возвращено " + books.length + " книг");
     }
 }
